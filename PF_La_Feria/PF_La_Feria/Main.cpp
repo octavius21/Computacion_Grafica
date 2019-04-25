776d60991d2caee959c581c5eb6103844d1dbeb6
Luis Octavio <octavio.12@hotmail.com>
2019-04-25 15:21:27 -0500

    Update Main.cpp

--- PF_La_Feria/PF_La_Feria/Main.cpp
+++ PF_La_Feria/PF_La_Feria/Main.cpp
@@ -1,1 +1,8 @@
-//Clase principal
+/*Clase principal
+**Facultad de Ingeneria
+**Autores: Sandoval Juarez Tania
+**			Gomez de la Cruz Luis Octavio
+**Materia:Computacion Grafica
+**Semestre:2019-2
+**Descripcion: Se realizara una feria tomando en cuanta anteriormente imagenes para su comparacion
+*/
